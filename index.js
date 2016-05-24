0626a8d8d35c43d26b024bba518c7243e42db107
wochap <gean.marroquin@gmail.com>
2016-05-24 11:49:25 -0500

    [add] initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import Offcanvas from './src/Offcanvas'
+import OffcanvasButton from './src/OffcanvasButton'
+import OffcanvasWrapper from './src/OffcanvasWrapper'
+
+export {
+  Offcanvas,
+  OffcanvasButton,
+  OffcanvasWrapper
+}
